bbc076be0a37ba90e6af9c8052287b5abb033e77
Pritika Vijayakumar <pritika.v2023@vitstudent.ac.in>
2025-11-02 14:51:37 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directories
+mkdir -p src #p means parameter
+mkdir -p research
+
+#creating files
+touch src/__init__.py #this is a constructor file
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb #notebook file
+touch requirements.txt
+
+echo "Directories and files created successfully"
